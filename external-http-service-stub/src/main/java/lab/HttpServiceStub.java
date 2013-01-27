c5adbbee3bca2bf4559d586bc3012ff05adb7e20
Vijay Patel <veejspeej@gmail.com>
2013-01-27 23:42:49 +0000

    a http stub deployment required by metrics demo app.

---
+++ external-http-service-stub/src/main/java/lab/HttpServiceStub.java
@@ -0,0 +1,19 @@
+package lab;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/myresource")
+public class HttpServiceStub {
+
+    @Path("/address")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response get() {
+        return Response.ok("123 Coventry Street").build();
+    }
+
+}
